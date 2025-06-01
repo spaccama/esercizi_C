a5d6094abb313432b262d63631e23a74a0865cf5
spacca <mattia.spaccarotella.work@gmail.com>
2025-06-01 19:14:48 +0200

    Create esercizio_02.c

---
+++ esercizi_base/esercizio_02.c
@@ -0,0 +1,17 @@
+/*Chiedi all'utente di inserire un numero e stabilisci se quel numero è pari o dispari*/
+
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    
+    printf("\nInserire un numero per verificare se e' pari o dispari: ");
+    scanf("%d", &num);
+
+    if(num%2==0) printf("\nIl numero %d e' pari\n", num);
+    if(num%2!=0) printf("\nIl numero %d e' dispari\n", num);
+
+    return 0;
+
+}
